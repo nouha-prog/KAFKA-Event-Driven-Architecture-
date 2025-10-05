16381fe1a6db0844e63d77f18a2f3758b7ee4bc5
Nouhaila <nouhailaelhajji488@gmail.com>
2025-10-05 18:19:19 +0100

    Modification

--- src/main/java/org/example/kafkaspringstream/handlers/PageEventHandler.java
+++ src/main/java/org/example/kafkaspringstream/handlers/PageEventHandler.java
@@ -1,4 +1,21 @@
 package org.example.kafkaspringstream.handlers;
 
+import org.example.kafkaspringstream.Events.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
 public class PageEventHandler {
+
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input)->{
+            System.out.println("*****************");
+            System.out.println(input.date());
+            System.out.println("******************");
+
+        };
+    }
 }
