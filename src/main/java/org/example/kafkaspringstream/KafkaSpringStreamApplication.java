4f6f9ab4a924d2ea262e24a3d421f7e4327c1318
Nouhaila <nouhailaelhajji488@gmail.com>
2025-10-05 14:51:15 +0100

    Premier commit de mon projet Kafka

---
+++ src/main/java/org/example/kafkaspringstream/KafkaSpringStreamApplication.java
@@ -0,0 +1,13 @@
+package org.example.kafkaspringstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kafkaspringstream/KafkaSpringStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kafkaspringstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
