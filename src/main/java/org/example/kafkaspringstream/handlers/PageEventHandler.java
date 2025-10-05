d62030243a1acf9fcab9e3c781f0980f08ecd5be
Nouhaila <nouhailaelhajji488@gmail.com>
2025-10-05 18:43:01 +0100

    Ajout du Supplier

--- src/main/java/org/example/kafkaspringstream/handlers/PageEventHandler.java
+++ src/main/java/org/example/kafkaspringstream/handlers/PageEventHandler.java
@@ -4,7 +4,10 @@ import org.example.kafkaspringstream.Events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
@@ -13,9 +16,22 @@ public class PageEventHandler {
     public Consumer<PageEvent> pageEventConsumer() {
         return (input)->{
             System.out.println("*****************");
-            System.out.println(input.date());
+            System.out.println(input.toString());
             System.out.println("******************");
 
+        };
+    }
+
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",
+                    Math.random()>0.5?"U1":"U2",
+                    new Date(),
+                    10+new Random().nextInt(10000)
+            );
         };
     }
 }
